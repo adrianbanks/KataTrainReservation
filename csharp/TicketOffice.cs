cb03c32ac6cac9b3dfff3fc97498f23d26a71536
Adrian Banks <github@adrianbanks.co.uk>
2013-10-25 11:54:33 +0100

    Add a test to check for the coach grouping logic

--- csharp/TicketOffice.cs
+++ csharp/TicketOffice.cs
@@ -5,21 +5,32 @@ namespace KataTrainReservation
 {
     public class TicketOffice
     {
+        private readonly IBookingIdService bookingIdService;
         private readonly IAvailableSeatsService availableSeatsService;
 
-        public TicketOffice(IAvailableSeatsService availableSeatsService)
+        public TicketOffice(IBookingIdService bookingIdService, IAvailableSeatsService availableSeatsService)
         {
+            this.bookingIdService = bookingIdService;
             this.availableSeatsService = availableSeatsService;
         }
 
         public Reservation MakeReservation(ReservationRequest request)
         {
             int numberOfRequestedSeats = request.SeatCount;
-
             var unreservedSeats = availableSeatsService.GetUnreservedSeats("");
+
             if (unreservedSeats.Count >= numberOfRequestedSeats)
             {
-                return new Reservation("", "id", unreservedSeats.Take(numberOfRequestedSeats).ToList());
+                var seatsGroupedByCoach = unreservedSeats.GroupBy(seat => seat.Coach);
+
+                foreach (var grouping in seatsGroupedByCoach)
+                {
+                    if (grouping.Count() >= numberOfRequestedSeats)
+                    {
+                        var reservedSeats = grouping.Take(numberOfRequestedSeats).ToList();
+                        return new Reservation("", bookingIdService.GetNextId(), reservedSeats);
+                    }
+                }
             }
 
             return new Reservation("", "", new List<Seat>());
--- csharp/TicketOfficeNUnitTest.cs
+++ csharp/TicketOfficeNUnitTest.cs
@@ -10,12 +10,22 @@ namespace KataTrainReservation
     {
         private const string trainId = "express_2013";
 
+        const string bookingId = "1234";
+
+        private static IBookingIdService MockBookingIdService(string bookingId)
+        {
+            var mockBookingIdService = Substitute.For<IBookingIdService>();
+            mockBookingIdService.GetNextId().Returns(bookingId);
+            return mockBookingIdService;
+        }
+
         [Test]
         public void MakeReservation_GivesReservationWithEmptyBookingID_WhenTheTrainIsFull()
         {
+            var mockBookingIdService = Substitute.For<IBookingIdService>();
             var mockAvailableSeatsService = Substitute.For<IAvailableSeatsService>();
             mockAvailableSeatsService.GetUnreservedSeats(trainId).ReturnsForAnyArgs(new List<Seat>());
-            var ticketOffice = new TicketOffice(mockAvailableSeatsService);
+            var ticketOffice = new TicketOffice(mockBookingIdService, mockAvailableSeatsService);
             var reservationRequest = new ReservationRequest(trainId, 1);
 
             var reservation = ticketOffice.MakeReservation(reservationRequest);
@@ -26,9 +36,11 @@ namespace KataTrainReservation
         [Test]
         public void MakeReservation_GivesReservationWithNonEmptyBookingID_WhenTheTrainIsNotFull_AndASingleSeatIsAskedFor()
         {
+
+            var mockBookingIdService = MockBookingIdService(bookingId);
             var mockAvailableSeatsService = Substitute.For<IAvailableSeatsService>();
             mockAvailableSeatsService.GetUnreservedSeats(trainId).ReturnsForAnyArgs(new List<Seat> {new Seat("A", 1)});
-            var ticketOffice = new TicketOffice(mockAvailableSeatsService);
+            var ticketOffice = new TicketOffice(mockBookingIdService, mockAvailableSeatsService);
             var reservationRequest = new ReservationRequest(trainId, 1);
 
             var reservation = ticketOffice.MakeReservation(reservationRequest);
@@ -39,10 +51,11 @@ namespace KataTrainReservation
         [Test]
         public void MakeReservation_GivesTheSeat_WhenOnlyOneSeatIsAvailable()
         {
+            var mockBookingIdService = Substitute.For<IBookingIdService>();
             var mockAvailableSeatsService = Substitute.For<IAvailableSeatsService>();
             var availableSeat = new Seat("A", 1);
             mockAvailableSeatsService.GetUnreservedSeats(trainId).ReturnsForAnyArgs(new List<Seat> {availableSeat});
-            var ticketOffice = new TicketOffice(mockAvailableSeatsService);
+            var ticketOffice = new TicketOffice(mockBookingIdService, mockAvailableSeatsService);
             var reservationRequest = new ReservationRequest(trainId, 1);
 
             var reservation = ticketOffice.MakeReservation(reservationRequest);
@@ -54,18 +67,60 @@ namespace KataTrainReservation
         [Test]
         public void MakeReservation_GivesTwoSeats_WhenMoreThanTwoSeatsAreAvailable()
         {
+            var mockBookingIdService = Substitute.For<IBookingIdService>();
             var mockAvailableSeatsService = Substitute.For<IAvailableSeatsService>();
             var availableSeat1 = new Seat("A", 1);
             var availableSeat2 = new Seat("A", 2);
             var availableSeat3 = new Seat("A", 3);
             mockAvailableSeatsService.GetUnreservedSeats(trainId).ReturnsForAnyArgs(
                 new List<Seat> {availableSeat1, availableSeat2, availableSeat3});
-            var ticketOffice = new TicketOffice(mockAvailableSeatsService);
+            var ticketOffice = new TicketOffice(mockBookingIdService, mockAvailableSeatsService);
             var reservationRequest = new ReservationRequest(trainId, 2);
 
             var reservation = ticketOffice.MakeReservation(reservationRequest);
 
             Assert.That(reservation.Seats.Count(), Is.EqualTo(2));
         }
+
+        [Test]
+        public void MakeReservation_GetsTheBookingIdFromTheBookingService_WhenASuccessfulBookingIsMade()
+        {
+            var mockBookingIdService = MockBookingIdService(bookingId);
+            var availableSeat1 = new Seat("A", 1);
+            var availableSeatsService = Substitute.For<IAvailableSeatsService>();
+            availableSeatsService.GetUnreservedSeats(trainId).ReturnsForAnyArgs(new List<Seat> {availableSeat1});
+            var ticketOffice = new TicketOffice(mockBookingIdService, availableSeatsService);
+
+            var reservationRequest = new ReservationRequest(trainId, 1);
+            
+            var reservation = ticketOffice.MakeReservation(reservationRequest);
+
+            Assert.That(reservation.BookingId, Is.EqualTo(bookingId));
+        }
+
+        [Test]
+        public void MakeReservation_GivesEmptyReservation_WhenThereAreNotEnoughSeatsInACoach()
+        {
+            var mockBookingIdService = MockBookingIdService(bookingId);
+            var availableSeat1 = new Seat("A", 1);
+            var availableSeat2 = new Seat("A", 2);
+            var availableSeat3 = new Seat("B", 1);
+            var availableSeat4 = new Seat("B", 2);
+            var availableSeatsService = Substitute.For<IAvailableSeatsService>();
+            availableSeatsService.GetUnreservedSeats(trainId).ReturnsForAnyArgs(
+                new List<Seat> {availableSeat1,availableSeat2,availableSeat3,availableSeat4});
+            var ticketOffice = new TicketOffice(mockBookingIdService, availableSeatsService);
+
+            var reservationRequest = new ReservationRequest(trainId, 4);
+            
+            var reservation = ticketOffice.MakeReservation(reservationRequest);
+
+            Assert.That(reservation.BookingId, Is.Empty);
+        }
+    }
+
+    public interface IBookingIdService
+    {
+        string GetNextId();
     }
 }
